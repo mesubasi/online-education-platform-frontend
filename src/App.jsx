fce4b656746c8c783bd356f99f5c50662cccf95b
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-01 18:50:34 +0300

    Route Added for Login,Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+
 function App() {
 
   return (
     <>
-      <div>
-        <p>Hello</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
