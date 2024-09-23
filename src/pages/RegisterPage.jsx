3644b1a094bcadd2162c1eeb1870c7bc33cfb955
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-23 23:21:41 +0300

    Register Page Added

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+//Register.jsx
 
-const RegisterPage = () => {
+import { Button, Form, Input, Carousel, message } from 'antd'
+import { Link } from 'react-router-dom'
+import AuthCarousel from '../../components/auth/AuthCarousel';
+import { useState } from 'react';
+
+const Register = () => {
+
     return (
-        <>
-            <div>
-                <h1>Register Page</h1>
+        <div className='h-screen'>
+            <div className='flex justify-between h-full'>
+                <div className='xl:px-20 px-10 w-full flex flex-col h-full justify-center relative'>
+                    <h1 className='text-center text-5xl font-bold mb-2'>LOGO</h1>
+                    <Form layout='vertical' size='large' onFinish={onFinish}>
+                        <Form.Item label="User Name" name={"username"} rules={[{ required: true, message: "Username Cannot Be Blank!" }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Email" name={"email"} rules={[{ required: true, message: "Email Cannot Be Blank!" }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Password" name={"password"} rules={[{ required: true, message: "Password Cannot Be Blank!" }]}>
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item label="Password Again" name={"passwordAgain"} dependencies={["password"]} rules={[{ required: true, message: "Password Again Cannot Be Blank!" }, ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error("The password that you entered do not match!"));
+                            },
+                        }),]}>
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type='primary' htmlType='submit' className='w-full' size='large' loading={loading}>Register</Button>
+                        </Form.Item>
+                    </Form>
+                    <div className='flex justify-center absolute left-0 bottom-10 w-full'>
+                        Do you have an account? &nbsp;
+                        <Link to="/login" className='text-blue-600'>
+                            Login Now</Link>
+                    </div>
+                </div>
+                <div className='xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff]'>
+                    <div className='w-full h-full flex items-center'>
+                        <div className='w-full'>
+                            <Carousel className='!h-full px-6' autoplay autoplaySpeed={3000}>
+                                <AuthCarousel img="/images/responsive.svg" title="Responsive" desc="Compatibility with All Device Sizes" />
+                                <AuthCarousel img="/images/statistic.svg" title="Statistics" desc="Expanded Statistics" />
+                                <AuthCarousel img="/images/customer.svg" title="Customer Satisfaction" desc="Customers Satisfied with the Product at the End of the Experience" />
+                                <AuthCarousel img="/images/admin.svg" title="Admin Panel" desc="One-Stop Management" />
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </div >
     )
 }
 
-export default RegisterPage
+export default Register
